3c06d2e714a4cf8a370623844d844a4eb27cbdf0
Pedro Bertani <pedro.bertani@fatec.sp.gov.br>
2021-10-19 10:28:53 -0300

    criando domain com cliente, adicionando referencias ao entityframework e mapeando domain como dbset

---
+++ AR.Domain/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AR.Domain
+{
+    public class Cliente
+    {
+
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [MaxLength(50, ErrorMessage = "O campo {0} deve ter no máximo {1} caracteres")]
+        public String Nome { get; set; }
+
+        public String CPF { get; set; }
+
+        public int Idade { get; set; }
+
+    }
+}
